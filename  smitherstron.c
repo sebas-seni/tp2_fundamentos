01a9dcd267909ea636f21c57a2fed241c31f3127
sebas-seni <ssenillosa94@gmail.com>
2025-05-26 13:18:43 +0000

    Cambios dia 1

---  smitherstron.c
+++  smitherstron.c
@@ -4,10 +4,21 @@
 #include <stdbool.h>
 #include "reporte.h"
 
+bool validar_cant_argumentos(int argc) {
+    return (argc == 2 || argc == 3);      
+}
 
+bool validar_arch_entrada(char *nombre_archivo) {
+    //Intentar abrir el archivo de entrada
+    //Validar formato, etc
+}
 
-int main() {
-    
+int main(int argc, char *argv[]) {
+
+    if (!validar_cant_argumentos(argc)) {
+        fprintf(stderr, "Cantidad de argumentos incorrecta. Ingrese 1 (uno) ó 2 (dos) archivos. \n");
+        return 1;
+    }
 
     return 0;
 }
